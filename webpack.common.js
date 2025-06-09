5d37d7f2ecdb046e61ff34ac9c697757c4647dc8
Momo <196245462+momoo038@users.noreply.github.com>
2025-06-09 16:47:04 +0200

    Changed webpack paths

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,11 @@
-// webpack.common.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin"); 
 
 module.exports = {
   entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-    }),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, "src/images"), 
-          to: "images",                             
-          noErrorOnMissing: true                   
-        }, 
-      ],
     }),
   ],
   module: {
@@ -26,12 +15,18 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
+        generator: {
+            filename: 'images/[name][ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+            filename: 'fonts/[name][ext]'
+        }
       },
     ],
   },
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,3 @@
-// webpack.prod.js
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
@@ -8,6 +7,7 @@ module.exports = merge(common, {
   output: {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: "/to-do/",
     clean: true,
   },
   devtool: "source-map",
